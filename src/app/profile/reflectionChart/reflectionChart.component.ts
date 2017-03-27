88eef668aa41c4237a954332db1da1975ab02ab7
andrewresearch <andrew@andrewresearch.net>
2017-03-27 21:11:05 +1000

    GoingOK_UI-4 Connect ngrx store to reflection chart

--- src/app/profile/reflectionChart/reflectionChart.component.ts
+++ src/app/profile/reflectionChart/reflectionChart.component.ts
@@ -2,12 +2,9 @@
  * Created by andrew on 6/06/2016.
  */
 
-import { Component, OnInit,Input,ElementRef,ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component,Input,ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
-import {
-    curveLinear, curveBasisOpen, curveCatmullRomOpen, curveStep, curveCatmullRom, curveBasis,
-    curveMonotoneX
-} from "d3-shape";
+import { curveBasis, curveMonotoneX} from "d3-shape"; //curveLinear, curveBasisOpen, curveCatmullRomOpen, curveStep, curveCatmullRom,
 import {ReflectionEntry} from "../../data/ReflectionStore";
 
 @Component({
@@ -16,7 +13,7 @@ import {ReflectionEntry} from "../../data/ReflectionStore";
     styleUrls: ['reflectionChart.component.css'],
     encapsulation: ViewEncapsulation.None
 })
-export class ReflectionChartComponent implements OnInit,OnChanges {
+export class ReflectionChartComponent {
 
     @Input('chartdata') chartdata:ReflectionEntry[];
     //chartData = [
@@ -29,37 +26,20 @@ export class ReflectionChartComponent implements OnInit,OnChanges {
     chartDataSize = 0;
 
     getChartData() {
-        //this.buildChart(this.elRef,this.chartdata);
         return JSON.stringify(this.chartdata);
     }
 
-    constructor() {} //(private elRef: ElementRef) {}
-
-    ngOnInit() {
-    }
+    constructor() {}
 
     ngDoCheck() {
         if (this.chartdata.length != this.chartDataSize) {
             this.chartDataSize = this.chartdata.length;
-            //this.buildChart(this.elRef,this.chartdata);
             this.buildChart(this.chartdata);
         }
     }
-    ngOnChanges() {
 
-    }
+    public buildChart(chartdata) {
 
-
-    ngAfterViewInit() {
-        //this.buildChart(this.elRef,this.chartdata);
-        this.buildChart(this.chartdata);
-    }
-
-
-
-    public buildChart(chartdata) { //(elRef: ElementRef,chartdata) {
-
-        //var element = elRef.nativeElement;
         var svg = null;
 
         var data:DatePoint[] = chartdata.map( r =>  {
@@ -69,13 +49,12 @@ export class ReflectionChartComponent implements OnInit,OnChanges {
             return dp;
         } );
 
-        var width = 1000;//element.parentElement.getBoundingClientRect.width //600; //element.parentElement.clientWidth;
+        var width = 1000;
         var height = width/4;
         var margin = {top: 10, right: 10, bottom: 20, left: 60};
         var w = width - margin.left - margin.right;
         var h = height - margin.top - margin.bottom;
 
-        //if(svg) svg.text('');
         var x = d3.scaleTime().range([0, w]);
         var y = d3.scaleLinear().range([h, 0]);
 
@@ -102,14 +81,6 @@ export class ReflectionChartComponent implements OnInit,OnChanges {
                 .attr("preserveAspectRatio", "xMinYMin meet")
                 .attr("viewBox", "0 0 "+1000+" "+4000)
                 .classed("svg-content", true);
-            // svg = d3.select(element)
-            //     .append("svg")
-            //     .attr("preserveAspectRatio","xMidYMid meet")
-            //     //.attr("width", w + margin.left + margin.right)
-            //     //.attr("height", h + margin.top + margin.bottom)
-            //     .append("g")
-            //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 
         svg.append("g")
             .attr("class", "x axis")
@@ -169,124 +140,3 @@ export class DatePoint {
     timestamp: Date;
     point: number;
 }
-
-
-
-/*
-export class ReflectionChartComponent implements OnInit {
-
-    @Input('chartdata') chartdata:Reflection[];
-    //chartData = { "generalPreposition": 7, "manner": 5, "consider": 21, "outcome": 15, "selfPossessive": 32, "emotive": 4, "temporal": 2, "definite": 23, "generalPronounVerb": 116, "pertains": 90, "selfReflexive": 14, "none": 7, "possible": 8, "compare": 2, "groupPossessive": 6, "anticipate": 6, "othersPossessive": 4 };
-
-    constructor(private elRef: ElementRef) { }
-
-    ngOnInit() {
-
-    }
-
-    ngAfterViewInit() {
-        this.buildChart(this.elRef,this.chartdata);
-        //console.log("chartdata is: "+this.chartdata);
-    }
-
-
-
-    public buildChart(elRef: ElementRef,chartdata:Reflection[]) {
-        let element = elRef.nativeElement;
-        let reflections = chartdata; //So that we can manipulate if needed
-        //var chart = d3.select(element.nativeElement);
-
-        var width = 600 //element.parent()[0].offsetWidth;
-        var height = width/4;
-        var margin = {top: 10, right: 10, bottom: 20, left: 22};
-        var w = width - margin.left - margin.right;
-        var h = height - margin.top - margin.bottom;
-
-        let svg = null;
-        console.log("reflections: ",reflections);
-        if(svg) svg.text('');
-        var x = d3.scaleTime().range([0, w]);
-        var y = d3.scaleLinear().range([h, 0]);
-
-        x.domain(d3.extent(reflections, function(ref) { return new Date(ref.timestamp); }));
-        y.domain([0,100]); // range of reflection points
-
-        var xAxis = d3.axisBottom(x)
-            .ticks(d3.timeWeeks); //,(x.domain()[1] - x.domain()[0]) > 15552e6 ? 2 : 1);
-
-        var yAxis = d3.axisLeft(y)
-            .tickValues([0,50,100]);
-        */
-/*
-        var hardline = d3.line()
-            .x(function(d,i) { return x(new Date(reflections[i].timestamp)); })
-            .y(function(d,i) { return y(parseFloat(reflections[i].reflection.point)); })
-            .curve(d3.curveLinear);
-            //.interpolate("linear");
-
-        var softline = d3.line()
-            .x(function(d,i) { return x(new Date(reflections[i].timestamp)); })
-            .y(function(d,i) { return y(parseFloat(reflections[i].reflection.point)); })
-            .curve(d3.curveBasisOpen);
-
-        var avg = d3.mean(reflections,function(d) { return y(parseFloat(d.reflection.point)); });
-        var meanline = d3.line()
-            .x(function(d,i) { return x(new Date(reflections[i].timestamp)); })
-            .y(function(d) { return avg; })
-            .curve(d3.curveLinear);
-*/
-/*
-        if(!svg) {
-            svg = d3.select(element[0])
-                .append("svg")
-                .attr("width", w + margin.left + margin.right)
-                .attr("height", h + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        }
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + h + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
-            */
-
-/*
-        svg.append("path")
-            .datum(reflections)
-            .attr("class", "meanline")
-            .attr("d", meanline);
-
-        svg.append("path")
-            .datum(reflections)
-            .attr("class", "softline")
-            .attr("d", softline);
-
-        svg.append("path")
-            .datum(reflections)
-            .attr("class", "hardline")
-            .attr("d", hardline);
-
-        svg.selectAll(".point")
-            .data(reflections)
-            .enter().append("svg:circle")
-            .attr("class","point")
-            .attr("r",5)
-            .attr("cx",function(d) { return x(new Date(d.timestamp)); })
-            .attr("cy",function(d) { return y(parseFloat(d.reflection.point)); })
-            .on('click', function(d) { alert("Reflection point "+ d.reflection.point+" at "+ d.timestamp); });
-*/
-// Everything setup - pass reflections to line as d
-
-
-        /*
-         .on('mouseover', function() { return d3.select(this).attr('r', 8);})
-         .on('mouseout',  function() { d3.select(this).attr('r', 4);})
-         .on('click', function(d, i) { console.log("d: "+d+" i: "+i );});
-         */
-
-
